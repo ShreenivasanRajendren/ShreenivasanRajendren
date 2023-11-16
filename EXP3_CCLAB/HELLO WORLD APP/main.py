ff719a0e8acdd963c35826fc5c1c0211805f82d9
Shreenivasan Rajendren <114797356+ShreenivasanRajendren@users.noreply.github.com>
2023-11-16 18:06:56 +0530

    Update main.py

--- EXP3_CCLAB/HELLO WORLD APP/main.py
+++ EXP3_CCLAB/HELLO WORLD APP/main.py
@@ -1,10 +1,8 @@
 from flask import Flask
-
-app= Flask(__name__)
-
+app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello World"
-    
-if __name__=='__main__':
-    app.run(host='127.0.0.1', port=8081, debug=True)
+"""Hello World!!!"""
+return 'ShreenivasanRajendren!'
+if __name__ == '__main__':
+app.run(host='127.0.0.1', port=8080, debug=True)
